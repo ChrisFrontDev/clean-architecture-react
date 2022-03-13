9267691e09ed24f61a9cde937d78064bc54db20d
Christian Pinho dos Santos <christiansantos.vortigo@moss.earth>
2022-03-13 19:28:23 -0300

    feat: ensure  RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
+import { HttpResponse } from './http-response'
+
 export type HttpPostClientParams = {
   url: string
   body?: object
 }
 
 export interface HttpPostClient {
-  post(params: HttpPostClientParams): Promise<void>
+  post(params: HttpPostClientParams): Promise<HttpResponse>
 }
