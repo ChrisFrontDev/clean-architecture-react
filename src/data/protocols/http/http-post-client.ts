794e3b99115cb235344e6c60f8f88d80f3a5dd4b
Christian Pinho dos Santos <christiansantos.vortigo@moss.earth>
2022-03-13 17:34:20 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,4 +1,10 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+export type HttpPostClientParams = {
+  url: string
+  body?: any
+}
+
 export interface HttpPostClient {
-  post(url: string): Promise<void>
+  post(params: HttpPostClientParams): Promise<void>
 }
